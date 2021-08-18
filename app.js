1d808c95dd90f312c64129890486a696fa3c6f75
HasibulRobin <hasibulrobin01914@gmil.com>
2021-08-18 20:22:56 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,34 @@
+function updateProductNumber(product, price, isIncreasing) {
+    const productInput = document.getElementById(product + '-number');
+    let productNumber = productInput.value;
+    if (isIncreasing) {
+        productNumber = parseInt(productNumber) + 1;
+    }
+    else if (productNumber > 0) {
+        productNumber = parseInt(productNumber) - 1;
+    }
+    productInput.value = productNumber;
+    // update total
+    const productTotal = document.getElementById(product + '-total');
+    productTotal.innerText = productInput.value * price;
+
+};
+
+// phone handle with event handler
+document.getElementById('phone-plus').addEventListener('click', function () {
+    updateProductNumber('phone', 1219, true)
+});
+
+document.getElementById('phone-minus').addEventListener('click', function () {
+    updateProductNumber('phone', 1219, false)
+});
+
+// case handle with event handler
+document.getElementById('case-plus').addEventListener('click', function () {
+
+    updateProductNumber('case', 59, true);
+});
+document.getElementById('case-minus').addEventListener('click', function () {
+
+    updateProductNumber('case', 59, false);
+})
